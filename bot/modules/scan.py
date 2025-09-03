9384d1a1c170a7e9b008feb8d7858f2e59e9f0a2
erroratservice <110653823+erroratservice@users.noreply.github.com>
2025-09-03 19:21:27 +0530

    Update scan.py

--- bot/modules/scan.py
+++ bot/modules/scan.py
@@ -5,10 +5,10 @@ from pyrogram.handlers import MessageHandler
 from pyrogram.errors import RPCError
 from pyrogram.types import Message
 
-from bot import bot, userbot, LOGGER, config_dict
-from bot.helper.ext_utils.bot_utils import get_readable_time, new_task, sync_to_async
+from bot import bot, userbot, LOGGER, config_dict, task_manager
+from bot.helper.ext_utils.bot_utils import get_readable_time, new_task
 from bot.helper.ext_utils.db_handler import db_handler
-from bot.helper.telegram_helper.message_utils import send_message, edit_message, send_status_message
+from bot.helper.telegram_helper.message_utils import sendMessage, editMessage, sendStatusMessage
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.mirror_leech_utils.status_utils.scan_status import ScanStatus
@@ -16,56 +16,58 @@ from bot.helper.mirror_leech_utils.status_utils.scan_status import ScanStatus
 @new_task
 async def scan_channel(_, message: Message):
     if len(message.command) < 2:
-        await send_message(message, f"<b>Usage:</b> /{BotCommands.ScanCommand} &lt;channel_id&gt;")
+        await sendMessage(message, f"<b>Usage:</b> /{BotCommands.ScanCommand} &lt;channel_id&gt;")
         return
 
     try:
-        channel_id = int(message.command[1])
-    except ValueError:
+        # Allow both channel username and ID
         channel_id = message.command[1]
+        if channel_id.startswith('@'):
+            channel_id = channel_id.replace('@', '')
+        else:
+            channel_id = int(channel_id)
+    except (ValueError, IndexError):
+        await sendMessage(message, "<b>Invalid channel ID or username.</b>")
+        return
 
     try:
         if userbot:
             await userbot.get_chat(channel_id)
             client = userbot
         else:
-            await bot.get_chat(channel_id)
-            client = bot
+            await sendMessage(message, "<b>USER_SESSION_STRING is not defined.</b>")
+            return
     except Exception as e:
-        await send_message(message, f"<b>Error:</b> {e}")
+        await sendMessage(message, f"<b>Error:</b> {e}")
         return
 
-    status_message = await send_message(message, "<i>Scanning channel...</i>")
-    start_time = time.time()
+    status_message = await sendMessage(message, "<i>Scanning channel...</i>")
     
     try:
         total_messages = await client.get_chat_history_count(channel_id)
     except Exception as e:
-        await edit_message(status_message, f"<b>Error getting message count:</b> {e}")
+        await editMessage(status_message, f"<b>Error getting message count:</b> {e}")
         return
 
-    scan_status = ScanStatus(status_message, total_messages, 0, start_time)
-    await send_status_message(message)
+    scan_status = ScanStatus(status_message, total_messages, 0, time.time())
+    await sendStatusMessage(message)
+    task_manager.add_task(scan_status)
     
     processed_messages = 0
     added_files = 0
     
     async for msg in client.get_chat_history(channel_id):
         processed_messages += 1
+        scan_status.processed = processed_messages
         if msg.caption:
             file_name = msg.caption.split('\n')[0].strip()
             if await db_handler.add_scanned_file(file_name):
                 added_files += 1
 
-        if processed_messages % 200 == 0:
-            scan_status.processed = processed_messages
-            # No need for a separate updater task, we can update directly in the loop
-            # This is more efficient than creating a separate async task for it.
-            
-    elapsed = time.time() - start_time
-    await edit_message(status_message, f"<b>Scan completed in {get_readable_time(elapsed)}.</b>\n\n"
+    elapsed = time.time() - scan_status.start_time()
+    task_manager.remove_task(scan_status.gid())
+    await editMessage(status_message, f"<b>Scan completed in {get_readable_time(elapsed)}.</b>\n\n"
                                        f"<b>Total messages scanned:</b> {processed_messages}\n"
                                        f"<b>New files added to database:</b> {added_files}")
-    scan_status.cancel() # To remove it from the status message
 
-bot.add_handler(MessageHandler(scan_channel, filters=filters.command(BotCommands.ScanCommand) & (CustomFilters.owner | CustomFilters.sudo)))
+bot.add_handler(MessageHandler(scan_channel, filters=filters.command(BotCommands.ScanCommand) & CustomFilters.authorized))
