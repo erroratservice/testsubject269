93597633e87a801bd94a07b44befb9b0bb99b0d6
erroratservice <110653823+erroratservice@users.noreply.github.com>
2025-09-09 13:11:44 +0530

    Update telegram_status.py

--- bot/helper/mirror_leech_utils/status_utils/telegram_status.py
+++ bot/helper/mirror_leech_utils/status_utils/telegram_status.py
@@ -1,20 +1,17 @@
-from bot import INCOMPLETE_TASK_NOTIFIER
-from ...ext_utils.status_utils import MirrorStatus, get_readable_file_size, get_readable_time
-# DbManager is no longer needed here
+from ...ext_utils.status_utils import (
+    MirrorStatus,
+    get_readable_file_size,
+    get_readable_time,
+)
+
 
 class TelegramStatus:
-
     def __init__(self, listener, obj, gid, status):
+        self.listener = listener
         self._obj = obj
+        self._size = self.listener.size
         self._gid = gid
         self._status = status
-        self.listener = listener
-        self._size = self.listener.size
-        # REMOVED broken database call from here
-
-    def __del__(self):
-        # REMOVED broken database call from here
-        pass
 
     def processed_bytes(self):
         return get_readable_file_size(self._obj.processed_bytes)
@@ -23,7 +20,7 @@ class TelegramStatus:
         return get_readable_file_size(self._size)
 
     def status(self):
-        if self._status == 'up':
+        if self._status == "up":
             return MirrorStatus.STATUS_UPLOAD
         return MirrorStatus.STATUS_DOWNLOAD
 
@@ -35,17 +32,17 @@ class TelegramStatus:
             progress_raw = self._obj.processed_bytes / self._size * 100
         except:
             progress_raw = 0
-        return f'{round(progress_raw, 2)}%'
+        return f"{round(progress_raw, 2)}%"
 
     def speed(self):
-        return f'{get_readable_file_size(self._obj.speed)}/s'
+        return f"{get_readable_file_size(self._obj.speed)}/s"
 
     def eta(self):
         try:
             seconds = (self._size - self._obj.processed_bytes) / self._obj.speed
             return get_readable_time(seconds)
         except:
-            return '-'
+            return "-"
 
     def gid(self):
         return self._gid
