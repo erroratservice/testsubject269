64f1ba48d4a725c87ca5cbdbab581611b900f2a8
erroratservice <110653823+erroratservice@users.noreply.github.com>
2025-09-09 18:19:58 +0530

    Update channel_leech.py

--- bot/modules/channel_leech.py
+++ bot/modules/channel_leech.py
@@ -23,6 +23,10 @@ def sanitize_filename(filename):
         r'\U0001F600-\U0001F64F'  # emoticons
         r'\U0001F300-\U0001F5FF'  # symbols & pictographs
         r'\U0001F680-\U0001F6FF'  # transport & map symbols
+        r'\U00002702-\U000027B0'  # dingbats
+        r'\U000024C2-\U0001F251'  # enclosed characters
+        r'\U0001F900-\U0001F9FF'  # supplemental symbols
+        r'\U0001FA00-\U0001FA6F'  # chess symbols
         r'\U0001F1E0-\U0001F1FF'  # flags (iOS)
         r'\u2600-\u26FF\u2700-\u27BF'  # misc symbols
         r']+', flags=re.UNICODE
